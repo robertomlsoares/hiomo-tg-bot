afac03d2c758f6321416358f85288262d6ded413
Roberto Soares <robertomlsoares@gmail.com>
2017-06-28 10:27:15 +0300

    Heroku configs

--- hiomo_bot.py
+++ hiomo_bot.py
@@ -9,6 +9,7 @@ from telegram.ext import Updater, CommandHandler, Job
 import logging
 import requests
 import datetime
+import os
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -201,7 +202,10 @@ def _get_menu_today():
 
 def main():
     #: Remember to remove the real token when pushing to GitHub.
-    updater = Updater('TOKEN')
+    TOKEN = 'TOKEN'
+    PORT = int(os.environ.get('PORT', '5000'))
+
+    updater = Updater(TOKEN)
 
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
@@ -215,7 +219,8 @@ def main():
 
     dispatcher.add_error_handler(error)
 
-    updater.start_polling()
+    updater.start_webhook(listen='0.0.0.0', port=PORT, url_path=TOKEN)
+    updater.bot.set_webhook('https://hiomo-bot.herokuapp.com/' + TOKEN)
     updater.idle()
 
 
